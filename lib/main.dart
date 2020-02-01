325ccc57004f87cab4838c69abc7faa4260706c2
Kevin Bowe <kevinbowe1999@gmail.com>
2020-01-31 19:53:23 -0500

    Add favorites screen and filters screen
    Add Delete function on meals

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,9 @@ import 'package:flutter/material.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
+import './screens/tabs_screen.dart';
+import './screens/filters_screen.dart';
+
 
 void main() => runApp(RecipeKeeper());
 
@@ -31,9 +34,10 @@ class RecipeKeeper extends StatelessWidget {
               )),
       initialRoute: '/',
       routes: {
-        '/': (ctx) => CategoriesScreen(),
+        '/': (ctx) => TabScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
       // onGenerateRoute: (settings) {
       //   print(settings.arguments);
